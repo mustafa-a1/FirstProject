7ad93cb0380aab9aa4e3268b957643de7fbd7d02
mustafa-a1 <m.abdul2048@gmail.com>
2024-01-06 12:13:13 -0500

    Initial Commit.

---
+++ src/com/smart/tests/Test.java
@@ -0,0 +1,12 @@
+package com.smart.tests;
+
+public class Test {
+
+	@org.testng.annotations.Test
+	public void loginTest() {
+
+		System.out.println("We are learning Git and Github through Eclipse");
+
+	}
+
+}
